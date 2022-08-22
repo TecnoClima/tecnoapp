2cc28a2ef1784fe995920cda7402aa1a4a6e028f
leomonay <leonardomonay@gmail.com>
2022-08-22 08:58:42 -0300

    add apiConfig.js

---
+++ src/apiConfig.js
@@ -0,0 +1,11 @@
+const appConfig = {
+  // url: "http://localhost:3001/v1",
+  url: "https://tecno-server.herokuapp.com/v1",
+  plantConfig: {
+    code: "SSN",
+  },
+};
+
+//line to check if gitignore worked.
+
+export { appConfig };
