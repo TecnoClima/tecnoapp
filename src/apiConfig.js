87128ef1d1508f2930b814bde9117d84907095f3
Lmonay <leonardo.monay@gmail.com>
2023-06-14 10:18:06 -0300

    fix server url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,7 @@
 const appConfig = {
   // url: "http://localhost:3001/v1",
-  url: "https://tecno-server.herokuapp.com/v1",
+  url: "http://200.105.69.248:3001/v1",
+  //url: "https://tecno-server.herokuapp.com/v1",
   plantConfig: {
     code: "SSN",
   },
