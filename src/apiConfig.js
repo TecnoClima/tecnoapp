dc2b8a642e2632eeca7b912560e54feb4f4726d6
Leo Monay <leonardomonay@gmail.com>
2026-03-09 23:59:54 -0300

    update prod serverUrl

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,10 +1,10 @@
-console.log("mode", process.env.REACT_APP_ENV);
+// console.log("mode", process.env.REACT_APP_ENV);
 
 const serverUrl = {
   dev: "http://localhost:3004/v1",
   // test: "http://localhost:3002/v1",
   test: "http://200.105.69.248:3002/v1",
-  prod: "http://200.105.69.248:3001/v1",
+  prod: "https://app.tecnoclima.com.ar/api/v1",
 };
 
 const appConfig = {
